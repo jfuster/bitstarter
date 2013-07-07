0c91a435960cba7252bfa340749fee871d0b60e7
Joan Fuster <joan.fuster@gmail.com>
2013-07-07 18:04:23 +0000

    web.js reads a string from the file index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+// Read the content of index.html and returns a buffer
+var index = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // Buffer to string
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
